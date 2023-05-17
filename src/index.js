8a67a0d773093eac3e8433a68bc699baf52c5bcd
NaVis0mple <109860906+NaVis0mple@users.noreply.github.com>
2023-05-17 23:45:04 +0800

    add done page render function

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { form } from './formEnter'
 import { submitbutton, itemList } from './add'
 
-// main  home show todolist
+
 const content = document.getElementById('content')
 
 
@@ -9,13 +9,32 @@ const content = document.getElementById('content')
 function createDeleteButton (index) {
     const button = document.createElement('button')
     button.classList.add(`no${index}`)
+    button.classList.add('delete')
     button.addEventListener('click', () => {
       itemList.splice(index,1)
       generateContent ()  
     })
     return button
   }
-//render new content
+//create a finished button,if click move list piece to done page
+const finishedList = []
+function createFinishedButton (index)  {
+  const button = document.createElement('button')
+  button.classList.add(`no${index}`)
+  button.classList.add('finished')
+  button.addEventListener('click',()=>{
+    const x = itemList.splice(index,1)[0] || {}
+    generateContent()
+    finishedList.push(x)
+    console.log(finishedList)
+    console.log(x)
+  })
+  return button
+}
+
+
+
+//render new content about itemList 
 function generateContent () {
     content.innerHTML = ''
   itemList.forEach((item,index) => {
@@ -24,14 +43,48 @@ function generateContent () {
     div1.innerHTML = `${item.title}  ${item.description} ${item.date}`
     div1.style.color = `${item.priorityT}`
     div1.style.backgroundColor = `${item.priorityB}`
-    const deletebutton = createDeleteButton(index)
-    div1.appendChild(deletebutton)
-    content.appendChild(div1)
-    
+    const deleteButton = createDeleteButton(index)
+    const finishedButton = createFinishedButton(index)
+    div1.appendChild(finishedButton)
+    div1.appendChild(deleteButton)
+    content.appendChild(div1)  
+})
+}
+//render finished list
+function generateFinishedContent () {
+  content.innerHTML = ''
+  finishedList.forEach((item,index) => {
+  const div1 = document.createElement('div')
+  div1.classList.add(`no${index}`)
+  div1.innerHTML = `${item.title}  ${item.description} ${item.date}`
+  div1.style.color = `${item.priorityT}`
+  div1.style.backgroundColor = `${item.priorityB}`
+  content.appendChild(div1)  
 })
 }
 
 
+// main  home show todolist
 submitbutton.addEventListener('click', () => {
   generateContent ()
 })
+
+//render sidebar todo page
+const sidebarTodoPage = document.querySelector('.todolist')
+sidebarTodoPage.addEventListener('click',()=>{
+  generateContent ()
+})
+
+//render sidebar done page
+const sidebarDonePage = document.querySelector('.done')
+sidebarDonePage.addEventListener('click',()=>{
+  generateFinishedContent ()
+})
+
+
+
+
+
+
+const sidebarHistorypage = document.querySelector('.history')
+
