65f7c7f4a03e5bf375cdb3fc6f2fccfb337a8ca9
NaVis0mple <109860906+NaVis0mple@users.noreply.github.com>
2023-05-17 22:24:39 +0800

    adjust deletebutton

--- src/index.js
+++ src/index.js
@@ -4,30 +4,34 @@ import { submitbutton, itemList } from './add'
 // main  home show todolist
 const content = document.getElementById('content')
 
-submitbutton.addEventListener('click', () => {
-    createContent ()
-})
+
 // create a button to delete the list piece
 function createDeleteButton (index) {
     const button = document.createElement('button')
-    content.appendChild(button)
-    button.classList.add(`no.${index}`)
+    button.classList.add(`no${index}`)
     button.addEventListener('click', () => {
       itemList.splice(index,1)
-      createContent ()  
+      generateContent ()  
     })
+    return button
   }
 //render new content
-function createContent () {
+function generateContent () {
     content.innerHTML = ''
   itemList.forEach((item,index) => {
     const div1 = document.createElement('div')
-    div1.classList.add(`no.${index}`)
+    div1.classList.add(`no${index}`)
     div1.innerHTML = `${item.title}  ${item.description} ${item.date}`
     div1.style.color = `${item.priorityT}`
-    div1.style.border = `solid 1px ${item.priorityB}`
     div1.style.backgroundColor = `${item.priorityB}`
+    const deletebutton = createDeleteButton(index)
+    div1.appendChild(deletebutton)
     content.appendChild(div1)
-    createDeleteButton(index)
+    
 })
 }
+
+
+submitbutton.addEventListener('click', () => {
+  generateContent ()
+})
